75deefcdf7cd32a9769927242c26a55bd44cbe9b
g w <gw@famefact.com>
2024-08-21 16:51:22 +0200

    Support both GET and POST methods in serverless function

--- api/transcribe.py
+++ api/transcribe.py
@@ -1,14 +1,16 @@
 from http.server import BaseHTTPRequestHandler
+import json
+
+def handle_request(handler):
+    handler.send_response(200)
+    handler.send_header('Content-type', 'application/json')
+    handler.end_headers()
+    response = json.dumps({"message": "Hello from the serverless function!"})
+    handler.wfile.write(response.encode())
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write("Hello, World!".encode())
-
-def do_POST(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    self.wfile.write("POST request received".encode())
+        handle_request(self)
+    
+    def do_POST(self):
+        handle_request(self)
