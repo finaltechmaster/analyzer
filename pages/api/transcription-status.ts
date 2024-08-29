e50a6a68dc844d9bc577f9dc7081a1eb20ad2c11
g w <gw@famefact.com>
2024-08-29 14:40:13 +0200

    Update vercel6

--- pages/api/transcription-status.ts
+++ pages/api/transcription-status.ts
@@ -1,13 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import AssemblyAI from 'assemblyai';
+import * as AssemblyAI from 'assemblyai';
 
 if (!process.env.ASSEMBLYAI_API_KEY) {
   throw new Error('ASSEMBLYAI_API_KEY is not set in the environment variables');
 }
 
-const client = new AssemblyAI({
-  apiKey: process.env.ASSEMBLYAI_API_KEY
-});
+const client = AssemblyAI.default.client(process.env.ASSEMBLYAI_API_KEY);
 
 // This line has been removed
 
